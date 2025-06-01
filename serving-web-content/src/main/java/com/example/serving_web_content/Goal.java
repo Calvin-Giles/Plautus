3bf4c7a56ea8e1248e93c07d983e3ad2f3ab59a7
Calvin-Giles <70451275+Calvin-Giles@users.noreply.github.com>
2025-06-01 15:02:34 -0700

    Spring
    Added all the files to a new Spring project
    Created a UI
    Needs work

---
+++ serving-web-content/src/main/java/com/example/serving_web_content/Goal.java
@@ -0,0 +1,37 @@
+package com.example.serving_web_content;
+
+import java.time.LocalDate;
+
+public class Goal {
+    private String name;
+    private double targetAmount;
+    private LocalDate targetDate;
+    private double currentSavings;
+    private String type; //Example: "Retirement" "Emergency Fund" "Vacation"
+
+    public Goal() {}
+
+    public Goal(String name, double targetAmount, LocalDate targetDate, double currentSavings, String type) {
+        this.name = name;
+        this.targetAmount = targetAmount;
+        this.targetDate = targetDate;
+        this.currentSavings = currentSavings;
+        this.type = type;
+    }
+
+    //Getters & Setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public double getTargetAmount() { return targetAmount; }
+    public void setTargetAmount(double targetAmount) { this.targetAmount = targetAmount; }
+
+    public LocalDate getTargetDate() { return targetDate; }
+    public void setTargetDate(LocalDate targetDate) { this.targetDate = targetDate; }
+
+    public double getCurrentSavings() { return currentSavings; }
+    public void setCurrentSavings(double currentSavings) { this.currentSavings = currentSavings; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+}
